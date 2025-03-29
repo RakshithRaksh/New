466ec38a3d8174e7bfe2c177f8ed3a9708b26aae
rak66 <rak66778844@gmail.com>
2025-03-29 13:05:28 +0530

    3/29/2025

---
+++ src/Helloworld.java
@@ -0,0 +1,11 @@
+public class Helloworld {
+
+    public static void main (String args[]){
+        int age = 66;
+        age = age+1;
+        age =78;
+        System.out.println(age);
+
+    }
+
+}
